274dbabd5dde9fd47fd54ab56b08650ad49a532a
Jameson Danning <jdanning@ucsc.edu>
2020-04-22 17:01:15 -0700

    added keys

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,8 +12,9 @@ class Play extends Phaser.Scene {
 
 
     create() {
+        keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.back1 = this.add.tileSprite(0,0,1280,720, 'background1').setOrigin(0,0);
-        this.player = this.add.sprite(0, 360, 'player').setOrigin(0,0);
+        this.player = this.physics.add.sprite(0, 355, 'player').setOrigin(0,0);
     }
 
     update() {
