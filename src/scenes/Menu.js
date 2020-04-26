df076feda3317b90e5067628954132caa0cbd197
Jameson Danning <jdanning@ucsc.edu>
2020-04-25 18:28:15 -0700

    menu + fade transition

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,15 +5,38 @@ class Menu extends Phaser.Scene {
 
     preload() {
         this.load.image('logo', './assets/logo.png');
+        this.load.image('start', './assets/start.png');
     }
 
 
     create() {
-        this.back1 = this.add.sprite(game.config.width/2,game.config.height/2, 'logo').setOrigin(0.5);
+        this.add.rectangle(0,0,1280,720, 0xFFFFFF).setOrigin(0,0);
+        this.cameras.main.backgroundColor = Phaser.Display.Color.HexStringToColor("#000000");
+        this.logo = this.add.sprite(game.config.width/2 + 330,game.config.height/2 - 180, 
+            'logo').setScale(0.5,0.5).setOrigin(0.5);
+
+        this.start = this.add.sprite(game.config.width/2 + 330,game.config.height/2 + 50, 
+                'start').setScale(0.5,0.5).setOrigin(0.5).setInteractive();
+
+        this.start.on('pointerdown', () => {
+
+            this.cameras.main.fade(2000, 0, 0, 0);
+            //this.scene.start("playScene");
+            
+        });
+
+    
+        this.cameras.main.on('camerafadeoutcomplete', () => {
+    
+            this.scene.start("playScene");
+    
+        });
     }
+    
 
     update() {
-        this.scene.start("playScene");
+        //this.scene.start("playScene");
         //this.back1.tilePositionX += 4;
     }
+
 }
