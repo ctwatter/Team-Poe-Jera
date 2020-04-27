4bba3ac348d2766bea8b01fc35b19292c22341ff
riko <ewidodo1998@gmail.com>
2020-04-26 23:16:38 -0700

    Removed collision after losing, reset scores

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -103,7 +103,11 @@ class Play extends Phaser.Scene {
             
         }
 
+        Score = 0; //reset score every playthrough
         this.score = this.add.text(10,0, 'Score: ' + Score, scoreConfig).setOrigin(0,0);
+
+        //game over flag
+        this.gameOver = false;
     }
 
     addBubble() {
@@ -135,52 +139,57 @@ class Play extends Phaser.Scene {
     }
 
     bubbleOverlap(player, bubble) {
-        if(bubble.good == 0){
-            //play sound here
-            this.sound.play('poof');
-            let cloudExParticles = this.add.particles('trail');
-            let cloudExEmitter1 = cloudExParticles.createEmitter({
-                alpha: { start: 1, end: 0 },
-                scale: { start: 0.1, end: 0 },
-                speedX: { min: -500, max: 500 },
-                speedY: { min: -500, max: 500 },
-                frequency: 5,
-                quantity: {min : 10, max: 10},
-                //angle: { min : 0, max : 360},
-                lifespan: 500
-            });
-            Score += 100;
+        if (!this.gameOver) {
+            if(bubble.good == 0){
+                //play sound here
+                this.sound.play('poof');
+                let cloudExParticles = this.add.particles('trail');
+                let cloudExEmitter1 = cloudExParticles.createEmitter({
+                    alpha: { start: 1, end: 0 },
+                    scale: { start: 0.1, end: 0 },
+                    speedX: { min: -500, max: 500 },
+                    speedY: { min: -500, max: 500 },
+                    frequency: 5,
+                    quantity: {min : 10, max: 10},
+                    //angle: { min : 0, max : 360},
+                    lifespan: 500
+                });
+                Score += 100;
 
-            cloudExEmitter1.explode(150, bubble.x, bubble.y);
+                cloudExEmitter1.explode(150, bubble.x, bubble.y);
 
-            //update score
-            bubble.resetLoc();
-            console.log("test");
-        } else {
-            if(Score > HighScore)
-            {
-                HighScore = Score;
+                //update score
+                bubble.resetLoc();
+                console.log("test");
+            } else {
+                //game over you made a booboo
+
+                if(Score > HighScore)
+                {
+                    HighScore = Score;
+                }
+
+                //do scene change
+                this.sound.play('bonk');
+                let cloudExParticles = this.add.particles('trail');
+                let cloudExEmitter1 = cloudExParticles.createEmitter({
+                    alpha: { start: 1, end: 0 },
+                    scale: { start: 0.1, end: 0 },
+                    speedX: { min: -500, max: 500 },
+                    speedY: { min: -500, max: 500 },
+                    frequency: 5,
+                    quantity: {min : 10, max: 10},
+                    //angle: { min : 0, max : 360},
+                    lifespan: 500
+                });
+                cloudExEmitter1.explode(150, bubble.x, bubble.y);
+                bubble.resetLoc();
+                this.gameOver = true;
+                this.cameras.main.fadeOut(2000,255, 255, 255);
+                this.cameras.main.on('camerafadeoutcomplete', () => {
+                    this.transitioning();
+                });
             }
-            //play sound here
-            //do scene change
-            this.sound.play('bonk');
-            let cloudExParticles = this.add.particles('trail');
-            let cloudExEmitter1 = cloudExParticles.createEmitter({
-                alpha: { start: 1, end: 0 },
-                scale: { start: 0.1, end: 0 },
-                speedX: { min: -500, max: 500 },
-                speedY: { min: -500, max: 500 },
-                frequency: 5,
-                quantity: {min : 10, max: 10},
-                //angle: { min : 0, max : 360},
-                lifespan: 500
-            });
-            cloudExEmitter1.explode(150, bubble.x, bubble.y);
-            bubble.resetLoc();
-            this.cameras.main.fadeOut(2000,255, 255, 255);
-            this.cameras.main.on('camerafadeoutcomplete', () => {
-                this.transitioning();
-            });
         }
     }
 
