30038a3749cef3378ce6f30ce08e8fbdd4aa23f8
Jameson Danning <jdanning@ucsc.edu>
2020-05-03 21:19:18 -0700

    updated comments

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
 /*
-Salary Team, by Jameson Danning, Colin Watterson, Enrico Widodo
+The Salaryman's Dream, by Jameson Danning, Colin Watterson, Enrico Widodo
 completed 5/2/2020
 
 For our game, our group decided to put in a few interesting technical and visual aspects.
 
-For technical aspects, we made it so that the player moves towards the position of the mouse
-cursor. We also implemented a local highscore system that keeps track of your highscore
-whenever you are not ingame. We also made it so that the transitions between the scenes happen
-seamlessly.
+Creative tilt:
+For technical aspects, we made it so that the player's movement is tweened towards the position of 
+the mouse cursor, allowing mouse movement that doesn't feel too jittery. We also made it so that the 
+transitions between the scenes happen seamlessly by fading in and out. Lastly, we also implemented a 
+local highscore system that keeps track of your highscore whenever you are not ingame.
 
 For visual aspects, all art assets used in the game sections were made by the talented Jameson D
 and the music is written by the talented Enrico W. We also managed to add a smooth rainbow
 trail that does not spit all the colors out at once for when the player collects a pickup,
 which the talented Colin W engineered. Our endless runner is definitely unconventional in
 the sense that its theme is atypical of endless runner games and the movement is unrestricted
-in all directions. We wanted this to be a representation of the salaryman experiencing freedom
+in all directions. We wanted this to be a representation of a Japanese salaryman experiencing freedom
 from his mountainload of work.
 */
 
